08ceebe8c93989ef643974ab783d23912f15e3da
ANTON <AntoniousEmad75@gmail.com>
2023-11-17 21:12:42 +0200

    create ahadeth branch

--- lib/tabs/ahadeth_tab.dart
+++ lib/tabs/ahadeth_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Ahadethtab extends StatelessWidget {
   const Ahadethtab({super.key});
@@ -7,6 +8,14 @@ class Ahadethtab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.purple,
+    );
+  }
+
+  void loadHadeth() async {
+    await rootBundle.loadString("assets/files/ahadeth.txt").then((
+        value) => null).catchError((e) {
+      print(e.toString());
+    }
     );
   }
 }
